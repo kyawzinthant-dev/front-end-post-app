356d0fb022a0f6a89e93f47ceca932cc20153ec5
kzinthant <kzinthant.d3v@gmail.com>
2020-11-09 16:06:22 +0630

    'postroughfinish'

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+  // posts = [
+  //   { title: 'First Post', content: 'This is first post content' },
+  //   { title: 'Second Post', content: 'This is second post content' },
+  //   { title: 'Third Post', content: 'This is third post content' },
+  //   { title: 'Fourth Post', content: 'This is fourth post content' },
+  // ]
+  @Input() posts: Post[] = [];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
